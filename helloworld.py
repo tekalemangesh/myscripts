4108c7b2c7cc8c83798c3289025983b5c1b2901b
tekalemangesh <tekalemangesh@gmail.com>
2017-07-19 02:14:35 +0530

    added first python script

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# getiing input from user and asking for there name and age
+
+print ('Hello world!')
+print ('please enter your name! ') # asking for name of the employee
+myName = input ()
+print ("it's goot to meet you, " + myName)
+print ('Hello ' + myName + ' please enter your age')
+myAge = input ()
+print ('Hello ' + myName + ' your age is ' + myAge + ' and you will be ' + str (int(myAge)+1) + 'in next year')
